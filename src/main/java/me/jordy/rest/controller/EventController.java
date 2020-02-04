e8cdd939593cc4f83c47263e5ff0a08fba8e0e68
bearstark <48487110+bearstark@users.noreply.github.com>
2020-02-05 04:09:00 +0900

    Add EventRepository and related test.

--- src/main/java/me/jordy/rest/controller/EventController.java
+++ src/main/java/me/jordy/rest/controller/EventController.java
@@ -1,7 +1,9 @@
 package me.jordy.rest.controller;
 
 import me.jordy.rest.entity.Event;
+import me.jordy.rest.entity.EventDto;
 import me.jordy.rest.repository.EventRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
@@ -20,17 +22,21 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
 
-    EventRepository eventRepository;
+    private final EventRepository eventRepository;
+    private final ModelMapper modelMapper;
 
     // 생성자로 생성시 빈으로 이미 등록되어 있으면 @Autowired를 붙일 필요가 없음.(Spring 4.4 이상 부터)
-    public EventController(EventRepository eventRepository){
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper){
         this.eventRepository = eventRepository;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
-
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+        Event event = modelMapper.map(eventDto, Event.class);
+        System.out.println("event = "+ event);
         Event newEvent = eventRepository.save(event);
+        System.out.println("newEvent = "+ newEvent);
 //        URI createdUri = linkTo(methodOn(EventController.class).createEvent(e)).slash("{id}").toUri();
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
 
--- src/test/java/me/jordy/rest/entity/EventControllerTest_WebMvcTest.java
+++ src/test/java/me/jordy/rest/entity/EventControllerTest_WebMvcTest.java
@@ -17,18 +17,19 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import javax.print.attribute.standard.Media;
-
 import java.time.LocalDateTime;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-public class EventControllerTest {
+
+// 웹 관련 빈만 등록! 좀 더 빠름.
+// 하지만 단위테스트라고 볼 수는 없음.
+// 디스패쳐 서블릿, 이벤트 컨트롤러, 핸들러, 매퍼, 컨버터 등등 모두 조합이 된 상태로 동작하는 테스트이기 떄문!
+@WebMvcTest
+public class EventControllerTest_WebMvcTest {
 
     // 스프링 MVC 테스트 핵심 클래스
     // 웹서버를 띄우지 않고도 스프링 MVC 가 요청을 처리하는 과정을 확인할 수 있기 때문에 컨트롤러 테스트용으로 자주 쓰임
@@ -39,6 +40,14 @@ public class EventControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    // 생성 사유
+    // 현재 테스트가 @WebMvcTest 로 실행 중.
+    // 그래서, EventRepository는 Bean 으로 등록 되지 않음.
+    // Mock으로 등록해서 빈이 존재 하지 않음 에러 해결.
+    // 하지만 여전히 Null을 반환.
+    @MockBean
+    EventRepository eventRepository;
 
     @Test
     public void createEvent() throws Exception {
@@ -58,6 +67,9 @@ public class EventControllerTest {
                 .offline(true)
                 .build()
                 ;
+
+
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events")
                     .contentType(MediaType.APPLICATION_JSON_UTF8)
