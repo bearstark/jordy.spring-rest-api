611ddf609ae0868cdb92cc8ad303be9d2a1cbfd3
bearstark <48487110+bearstark@users.noreply.github.com>
2020-02-05 02:05:47 +0900

    Project Init

---
+++ src/main/java/me/jordy/rest/BaekRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.jordy.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaekRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaekRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jordy/rest/BaekRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jordy.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaekRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
